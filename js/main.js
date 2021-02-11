bf58d05a8e870c1d14c792fe7b9a0644a44e11eb
вашÐАнтон <gluskov352@gmail.com>
2021-02-11 16:50:10 +0300

    Задание №2 - Нужно больше функций (1.1)

--- js/main.js
+++ js/main.js
@@ -3,23 +3,23 @@ let maxNumber = 20;
 
 const anyNumber = function(min, max){
   if(min >= max || max <= min || min < 0 || max < 0) {
-    console.log('Ошибка - скоректиуйте введенные числа');
+    throw(new Error())
   }
   return  Math.floor(Math.random() * (max - min) + min);
 }
 
-console.log(anyNumber(minNumber, maxNumber));
+anyNumber(minNumber, maxNumber);
 
-let minPosition = 1222;
+let minPosition = 2;
 let maxPosition = 222;
 let outputNumber = 0;
 
 const anyN = function(min, max){
   if(min >= max || max <= min || min < 0 || max < 0) {
-    console.log('Ошибка - скоректиуйте введенные числа');
+    throw(new Error())
   }
   outputNumber  = Math.random() * (max - min) + min;
   return outputNumber.toFixed(1)
 }
 
-console.log(anyN(minPosition, maxPosition));
+anyN(minPosition, maxPosition);
